4044f6787bb3e7384ab55ecdc9e40d651b21d02c
jnszkr <joszkar@gmail.com>
2019-01-15 23:02:08 +0100

    fixed documentation

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,18 @@ pub type LcsToken = String;
 pub type LcsSeq = Vec<LcsToken>;
 pub type LcsDelimiters = Vec<char>;
 
-/** 
- * Tokenizer is using delimiters specified in LcsMap::new to create the tokens.
-*/
+/// # Examples
+///
+/// ```
+/// let tokens : Vec<_> = spell::tokenize("a,b c", &[' ', ',']).collect();
+///
+/// assert_eq!(vec!["a", "b", "c"], tokens);
+/// ```
 pub fn tokenize<'a>(input: &'a str, delimiters: &'a [char]) -> impl Iterator<Item = &'a str> {
-    input.trim().split(&delimiters[..]).filter(|s| !s.is_empty())
+    input
+        .trim()
+        .split(&delimiters[..])
+        .filter(|s| !s.is_empty())
 }
 
 #[cfg(test)]
