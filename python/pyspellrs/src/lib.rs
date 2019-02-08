64d92d63ab392bfa9cfbf2c367c2f121983f65bc
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-02-08 16:52:50 -0500

    Add map constructor

--- python/pyspellrs/src/lib.rs
+++ python/pyspellrs/src/lib.rs
@@ -1,3 +1,8 @@
+use spell::map::LcsMap;
+
+pub struct _Map {
+    map: LcsMap,
+}
 
 fn _init_env_logger() {
     std::env::var("RUST_LOG")
@@ -18,6 +23,37 @@ fn _init_env_logger() {
 pub unsafe extern "C" fn init_env_logger() {
     _init_env_logger();
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn new_map() -> *const _Map {
+    log::debug!("Creating new map...");
+    let map = LcsMap::new();
+
+    let map_ptr = Box::into_raw(Box::new(_Map { map })) as *const _Map;
+    log::debug!("Created map: {:?}", map_ptr);
+
+    map_ptr
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn free_map(map_ptr: *const _Map) {
+    log::debug!("Attempting to drop map {:?}", map_ptr);
+    if map_ptr.is_null() {
+        log::warn!("Attempted to drop null pointer. Skipping.");
+    } else {
+        log::debug!("Dropping map...");
+        let map = Box::from_raw(map_ptr as *mut _Map);
+        std::mem::drop(map);
+    }
+}
+
+// #[no_mangle]
+// pub unsafe extern "C" fn with_delimiters() -> *const _Map {
+//     // delimiters: Vec<char>
+//     // let map = LcsMap::with_delimiters(vec![' ', ',']);
+
+//     Box::into_raw(Box::new(_Map { map })) as *const _Map
+// }
 
 
 #[cfg(test)]
