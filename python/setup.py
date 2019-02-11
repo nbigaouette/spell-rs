f198e53c0f6cc56f023afb9dba6f9c88baecf3e8
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-02-11 15:54:44 -0500

    Use absolute directories for paths

--- python/setup.py
+++ python/setup.py
@@ -24,9 +24,12 @@ def build_native(spec):
         cmd.append('--release')
     cmd.append('--package')
     cmd.append('pyspellrs')
+
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    add_external_build_path = os.path.realpath(os.path.dirname(file_path))
     build = spec.add_external_build(
         cmd=cmd,
-        path='..'
+        path=add_external_build_path
     )
 
     # Step 2: add a cffi module based on the dylib we built
